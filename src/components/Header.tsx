137d684d7c03fa0c13e67224d3b1b391ff4834c3
luizengdev <luizengdev@gmail.com>
2024-02-03 18:37:40 -0300

    feat: add input, datepicker and currency input components

---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+  const { status, data } = useSession();
+
+  const handleLoginClick = () => signIn();
+  const handleLogoutClick = () => {
+    setMenuIsOpen(false);
+    signOut();
+  };
+
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
+
+  return (
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <Image src="/logo.png" width={50} height={32} alt="Logo" />
+        <h1 className="text-primary text-xl font-semibold">Next Trip</h1>
+      </div>
+
+      {status === "unauthenticated" && (
+        <button
+          className="text-primary text-sm font-semibold"
+          onClick={handleLoginClick}
+        >
+          Login
+        </button>
+      )}
+
+      {status === "authenticated" && data.user && (
+        <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 px-3 relative">
+          <AiOutlineMenu
+            size={16}
+            onClick={handleMenuClick}
+            className="cursor-pointer"
+          />
+
+          <Image
+            height={35}
+            width={35}
+            src={data.user.image!}
+            alt={data.user.name!}
+            className="rounded-full shadow-md"
+          />
+
+          {menuIsOpen && (
+            <div className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <button
+                className="text-primary text-xs font-semibold"
+                onClick={handleLogoutClick}
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
