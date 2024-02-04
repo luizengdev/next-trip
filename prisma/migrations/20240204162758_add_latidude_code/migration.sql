5dea0c2f5f26caf84cdf73a3a9498fc27fe8895e
luizengdev <luizengdev@gmail.com>
2024-02-04 13:28:16 -0300

    feat: add seed script and new migration

--- prisma/migrations/20240204162758_add_latidude_code/migration.sql
+++ prisma/migrations/20240204162758_add_latidude_code/migration.sql
@@ -1,13 +1,13 @@
 /*
   Warnings:
 
-  - Added the required column `latidute` to the `Trip` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `latitude` to the `Trip` table without a default value. This is not possible if the table is not empty.
   - Added the required column `locationDescription` to the `Trip` table without a default value. This is not possible if the table is not empty.
   - Added the required column `longitude` to the `Trip` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Trip" ADD COLUMN     "latidute" DOUBLE PRECISION NOT NULL,
+ALTER TABLE "Trip" ADD COLUMN     "latitude" DOUBLE PRECISION NOT NULL,
 ADD COLUMN     "locationDescription" TEXT NOT NULL,
 ADD COLUMN     "longitude" DOUBLE PRECISION NOT NULL;
 
