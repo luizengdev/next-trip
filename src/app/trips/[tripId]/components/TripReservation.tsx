d960ee2386f6e7f55d595765917b944957bc1545
luizengdev <luizengdev@gmail.com>
2024-02-05 22:00:41 -0300

    feat: add total price to trip

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/Button";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
+import { differenceInDays } from "date-fns";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
@@ -10,6 +11,7 @@ interface TripReservationProps {
   tripStartDate: Date;
   tripEndDate: Date;
   maxGuests: number;
+  pricePerDay: number;
 }
 
 interface TripReservationForm {
@@ -22,6 +24,7 @@ const TripReservation = ({
   maxGuests,
   tripStartDate,
   tripEndDate,
+  pricePerDay,
 }: TripReservationProps) => {
   const {
     register,
@@ -36,6 +39,7 @@ const TripReservation = ({
   };
 
   const startDate = watch("startDate");
+  const endDate = watch("endDate");
 
   return (
     <div className="flex flex-col px-5">
@@ -109,7 +113,11 @@ const TripReservation = ({
 
       <div className="flex justify-between mt-3">
         <p className="font-medium text-sm text-primaryDarker">Total: </p>
-        <p className="font-medium text-sm text-primaryDarker">R$ 2500</p>
+        <p className="font-medium text-sm text-primaryDarker">
+          {startDate && endDate
+            ? `R$ ${differenceInDays(endDate, startDate) * pricePerDay}.00`
+            : ""}
+        </p>
       </div>
 
       <div className="pb-10 border-b border-grayLighter w-full">
