f8be52a26c882ce815d8ce6e92be77f0378ab44a
luizengdev <luizengdev@gmail.com>
2024-02-09 20:04:55 -0300

    feat: add Modal components

---
+++ src/app/my-trips/components/Modal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Modal = ({ children }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-white p-6 rounded-lg w-4/5 md:w-1/2">{children}</div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
