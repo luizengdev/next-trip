9bddcfa0907db2e7c88c61cf6a63185d126168c3
luizengdev <luizengdev@gmail.com>
2024-02-09 22:44:00 -0300

    style: adapt home pag to dekstop

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -31,12 +31,12 @@ const TripSearch = () => {
   };
 
   return (
-    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
-      <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
 
-      <div className="flex flex-col gap-4 mt-5 ">
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
         <Input
           placeholder="Onde você quer ir?"
           error={!!errors.text}
@@ -49,7 +49,7 @@ const TripSearch = () => {
           })}
         />
 
-        <div className="flex gap-4 ">
+        <div className="flex gap-4 lg:w-full">
           <Controller
             name="startDate"
             control={control}
@@ -80,6 +80,7 @@ const TripSearch = () => {
         </div>
 
         <Button
+          className="w-1/2 lg:h-fit"
           onClick={() => handleSubmit(onSubmit)()}
           aria-label="Buscar viagem"
         >
