506b5b4871a28ff7a85f1dec29ff54c793f65535
luizengdev <luizengdev@gmail.com>
2024-02-14 18:02:53 -0300

    style: add hover image

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -12,7 +12,13 @@ type IconProps = {
 const Icon: FC<IconProps> = ({ path, name, alt, href }) => {
   return (
     <Link href={href} className="flex flex-col items-center gap-2">
-      <Image src={path} width={35} height={35} alt={alt} />
+      <Image
+        src={path}
+        width={35}
+        height={35}
+        alt={alt}
+        className="hover:scale-110 transition-all"
+      />
       <span className="text-GrayPrimaryLighter hover:text-primary lg:text-base">
         {name}
       </span>
